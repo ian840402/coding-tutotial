a7da1d8784f62606406f75b1fb690786f35e0cd4
Ian.Zheng <ian.zheng840402@gmail.com>
2020-08-05 11:22:32 +0800

    第一週課題

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,24 @@
-// // import images
-// const requireContext = require.context("../images", true, /^\.\/.*\.png$|^\.\/.*\.jp?g$|/);
-// requireContext.keys().map(requireContext);
+const blockOne = document.querySelector('#block-1')
+const blockTwo = document.querySelector('#block-2')
+const blockThree = document.querySelector('#block-3')
+const blockWrap = document.querySelector('#block-wrap')
 
+let count = 0
+let amount = 0
 
-class Animal {
-  constructor(name) {
-      this.name = name;
-  }
-  getName() {
-      return this.name;
-  }
-}
+blockOne.addEventListener('click', (e) => {
+  console.log('你點擊了一次區塊一')
+})
 
-const dog = new Animal('dog');
+blockTwo.addEventListener('click', (e) => {
+  count++
+  console.log(`你點擊了 ${count} 次`)
+})
+
+blockThree.addEventListener('click', (e) => {
+  const newDom = document.createElement('div')
+  amount++
+  newDom.innerHTML = `這是第 ${amount} 個新區塊`
+  newDom.classList.add('new-block')
+  blockWrap.appendChild(newDom)
+})
