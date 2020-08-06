176c1038a421d3e4508d14372396b67c607d02cb
Ian.Zheng <ian.zheng840402@gmail.com>
2020-08-06 13:54:41 +0800

    調整code

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,23 @@ const blockWrap = document.querySelector('#block-wrap')
 let count = 0
 let amount = 0
 
-blockOne.addEventListener('click', (e) => {
-  console.log('你點擊了一次區塊一')
-})
+const blockOneClickHandler = (e) => console.log('你點擊了一次區塊一')
 
-blockTwo.addEventListener('click', (e) => {
+const blockTwoClickHandler = (e) => {
   count++
   console.log(`你點擊了 ${count} 次`)
-})
+}
 
-blockThree.addEventListener('click', (e) => {
+const blockThreeClickHandler = (e) => {
   const newDom = document.createElement('div')
   amount++
   newDom.innerHTML = `這是第 ${amount} 個新區塊`
   newDom.classList.add('new-block')
   blockWrap.appendChild(newDom)
-})
+}
+
+blockOne.addEventListener('click', blockOneClickHandler, false)
+
+blockTwo.addEventListener('click', blockTwoClickHandler, false)
+
+blockThree.addEventListener('click', blockThreeClickHandler, false)
